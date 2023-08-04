34ec5c8114c1aa2736ff35b2c1661687ca3d60d6
Abhirup Bakshi <abhirup.bakshi@disroot.org>
2023-08-04 18:27:31 +0530

    modified:   back-end/index.js

--- back-end/index.js
+++ back-end/index.js
@@ -16,6 +16,39 @@ app.get('/', (req, res) => {
     res.status(200).send('Welcome To Open AI');
 })
 
+async function getResponse(prompt) {
+    
+    try {
+
+        const response = await fetch('https://api.openai.com/v1/engines/text-davinci-003/completions', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': `Bearer ${apiKey}`,
+                'Access-Control-Allow-Origin': '*'
+            },
+            body: JSON.stringify({
+                prompt,
+                max_tokens: 150,  // Adjust as needed
+            }),
+        });
+
+        const responseData = await response.json();
+        const generatedCode = responseData.choices[0].text.trim();
+
+        console.log(`generatedCode `, generatedCode);
+
+        return generatedCode;
+
+
+    } 
+    catch (error) {
+
+        console.error('Error converting code:', error);
+        return error;
+    }
+}
+
 app.post('/converter', async (req, res) => {
 
     const { inputCode, sourceLanguage, targetLanguage } = req.body;
@@ -26,42 +59,10 @@ app.post('/converter', async (req, res) => {
     } else {
         prompt = `Convert the following code into ${targetLanguage}:\n${inputCode}`;
     }
-
-    try {
-
-        const response = await fetch('https://api.openai.com/v1/engines/text-davinci-003/completions', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-                'Authorization': `Bearer ${apiKey}`,
-                'Access-Control-Allow-Origin': '*'
-            },
-            body: JSON.stringify({
-                prompt,
-                max_tokens: 150,  // Adjust as needed
-            }),
-        });
-
-        const responseData = await response.json();
-        const generatedCode = responseData.choices[0].text.trim();
-
-        console.log(`generatedCode `, generatedCode);
-
-        return res.status(201).send({
-            data: generatedCode
-        })
-
-
-    } catch (error) {
-
-        console.error('Error converting code:', error);
-
-        return res.status(201).send({
-            data: error
-        })
-
-    }
-
+    
+    return res.status(400).send({
+        data: await getResponse(prompt)
+    })
 })
 
 
@@ -71,40 +72,9 @@ app.post('/debug', async (req, res) => {
 
     const prompt = `debug the following code (which could include identification of errors, suggestions for fixes, etc.) : \n${inputCode}`;
 
-    try {
-
-        const response = await fetch('https://api.openai.com/v1/engines/text-davinci-003/completions', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-                'Authorization': `Bearer ${apiKey}`,
-                'Access-Control-Allow-Origin': '*'
-            },
-            body: JSON.stringify({
-                prompt,
-                max_tokens: 150,  // Adjust as needed
-            }),
-        });
-
-        const responseData = await response.json();
-        const generatedCode = responseData.choices[0].text.trim();
-
-        console.log(`generatedCode `, generatedCode);
-
-        return res.status(201).send({
-            data: generatedCode
-        })
-
-
-    } catch (error) {
-
-        console.error('Error converting code:', error);
-
-        return res.status(201).send({
-            data: error
-        })
-
-    }
+    return res.status(400).send({
+        data: await getResponse(prompt)
+    })
 
 })
 
@@ -115,41 +85,9 @@ app.post('/performance', async (req, res) => {
 
     const prompt = `check performance of the following code and assessment of the code's quality (such as commentary on style, organization, potential improvements, etc.) : \n${inputCode}`;
 
-    try {
-
-        const response = await fetch('https://api.openai.com/v1/engines/text-davinci-003/completions', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-                'Authorization': `Bearer ${apiKey}`,
-                'Access-Control-Allow-Origin': '*'
-            },
-            body: JSON.stringify({
-                prompt,
-                max_tokens: 150,  // Adjust as needed
-            }),
-        });
-
-        const responseData = await response.json();
-        const generatedCode = responseData.choices[0].text.trim();
-
-        console.log(`generatedCode `, generatedCode);
-
-        return res.status(201).send({
-            data: generatedCode
-        })
-
-
-    } catch (error) {
-
-        console.error('Error converting code:', error);
-
-        return res.status(201).send({
-            data: error
-        })
-
-    }
-
+    return res.status(400).send({
+        data: await getResponse(prompt)
+    })
 })
 
 app.listen(3000, () => {
